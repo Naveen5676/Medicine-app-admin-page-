7ac84814c35864d330c4083d51d8e7819400c009
Naveen5676 <sample.first@yahoo.com>
2023-11-22 00:50:28 +0530

    "change input code"

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -1,77 +1,45 @@
-import React, { Fragment, useContext, useState } from "react";
+import React, { Fragment, useContext } from "react";
 import classes from './Input.module.css';
 import DataContext from "../Store/Datacontext";
 
 const Input = () => {
     const ctxdata = useContext(DataContext);
-    const [formData, setFormData] = useState({
-        medicineName: "",
-        description: "",
-        price: 0,
-        quantity: 0
-    });
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        // Access form values directly from the form's DOM elements
+        const medicineName = e.target.elements.medicineName.value;
+        const description = e.target.elements.description.value;
+        const price = parseFloat(e.target.elements.price.value);
+        const quantity = parseInt(e.target.elements.quantity.value);
+
         // Create an item object with the form data
         const newItem = {
-            medicineName: formData.medicineName,
-            description: formData.description,
-            price: formData.price,
-            quantity: formData.quantity
+            medicineName,
+            description,
+            price,
+            quantity
         };
 
         // Call the addItem function from the context to add the new item
         ctxdata.addItem(newItem);
 
-        // Reset the form after submission
-        setFormData({
-            medicineName: "",
-            description: "",
-            price: 0,
-            quantity: 0
-        });
+        // Reset the form after submission (optional)
+        e.target.reset();
     };
 
     return (
         <Fragment>
             <form onSubmit={handleSubmit} className={classes.form}>
                 <label>Medicine Name</label>
-                <input
-                    type="text"
-                    name="medicineName"
-                    value={formData.medicineName}
-                    onChange={handleInputChange}
-                />
+                <input type="text" name="medicineName" />
                 <label>Description</label>
-                <input
-                    type="text"
-                    name="description"
-                    value={formData.description}
-                    onChange={handleInputChange}
-                />
+                <input type="text" name="description" />
                 <label>Price</label>
-                <input
-                    type="number"
-                    name="price"
-                    value={formData.price}
-                    onChange={handleInputChange}
-                />
+                <input type="number" name="price" />
                 <label>Quantity Available</label>
-                <input
-                    type="number"
-                    name="quantity"
-                    value={formData.quantity}
-                    onChange={handleInputChange}
-                />
+                <input type="number" name="quantity" />
                 <button type='submit'>Submit</button>
             </form>
         </Fragment>
